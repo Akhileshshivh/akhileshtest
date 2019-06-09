6336e3d913d15914f17d729ab2722276ac6751f0
Akhileshshivh <50072006+Akhileshshivh@users.noreply.github.com>
2019-06-09 16:03:16 +0530

    cm
    c test

---
+++ test.php
@@ -0,0 +1,20 @@
+
+Url:https://stackoverflow.com/questions/28528963/custom-success-message-after-submitting-comment-on-wordpress
+
+add_action( 'set_comment_cookies', function( $comment, $user ) {
+    setcookie( 'ta_comment_wait_approval', '1', 0, '/' );
+}, 10, 2 );
+
+add_action( 'init', function() {
+    if( isset( $_COOKIE['ta_comment_wait_approval'] ) && $_COOKIE['ta_comment_wait_approval'] === '1' ) {
+        setcookie( 'ta_comment_wait_approval', '0', 0, '/' );
+        add_action( 'comment_form_before', function() {
+            echo "<p id='wait_approval' style='padding-top: 40px;'><strong>Your comment has been sent successfully lepide.</strong></p>";
+        });
+    }
+});
+
+add_filter( 'comment_post_redirect', function( $location, $comment ) {
+    $location = get_permalink( $comment->comment_post_ID ) . '#wait_approval';
+    return $location;
+}, 10, 2 );
